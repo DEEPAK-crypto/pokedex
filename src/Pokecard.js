0062121509bfe9bddf602d4d29eda894da7f9407
Deepak Singh <deepak0064.cse19@chitkara.edu.in>
2020-07-18 23:34:20 +0530

    changed images

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
-const PokeApi = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+//const PokeApi = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const PokeApi = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component {
 	render() {
-		let imgSrc = `${PokeApi}${this.props.id}.png`;
+		let imgSrc = `${PokeApi}${padToThree(this.props.id)}.png`;
 		return (
 			<div className="Pokecard">
 				<h1 className="Pokecard-title">{this.props.name}</h1>
